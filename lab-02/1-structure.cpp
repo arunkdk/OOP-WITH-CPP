581cefd9bb6959efa9f8a712699a4ae74b9ff960
vayu-01 <arunkdk246@gmail.com>
2025-04-21 22:02:24 +0545

    add first program of lab-02

---
+++ lab-02/1-structure.cpp
@@ -0,0 +1,38 @@
+/*
+Write a program to create a structure in C++ named Hospital giving details about the patient admitted. Details are:
+• Patient name
+• Age
+• Gender
+• Nature of illness
+*/
+
+#include<iostream>
+using namespace std;
+struct  Hospital
+{
+    char name[20];
+    int age;
+    char gender[10];
+    char illness[30];
+};
+int main()
+{
+    Hospital p;
+    cout<<"Enter the information about patient:"<<endl;
+    cout<<"Name:";
+    cin.getline(p.name,20);
+    cout<<"Age:";
+    cin>>p.age;
+    cin.ignore();           //ignores the leftover characters in the input buffer.
+    cout<<"Gender:";
+    cin.getline(p.gender,10);
+    cout<<"Nature of illness:";
+    cin.getline(p.illness,30);
+    cin.get();          // Waits for Enter key before closing the terminal
+    cout<<"Details of patient:"<<endl;
+    cout<<"Name:"<<p.name<<endl;
+    cout<<"Age:"<<p.age<<endl;
+    cout<<"Gender:"<<p.gender<<endl;
+    cout<<"Nature of illness:"<<p.illness;
+    return 0;
+}
